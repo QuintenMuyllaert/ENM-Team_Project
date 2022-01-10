53c2e5e6320917298fc4e68bb204aae41d282690
Toby Bostoen <toby.bostoen@student.howest.be>
2022-01-10 14:32:58 +0100

    styling electrical page

--- www/electricalChart.js
+++ www/electricalChart.js
@@ -10,15 +10,27 @@ const drawChart = (data) => {
       labels: ["first", "second", "third", "fourth", "fifth", "sixth", "seventh"],
       datasets: [
         {
+          label: "kW/jaar",
           barPercentage: 0.9,
           barThickness: 20,
           maxBarThickness: 8,
           minBarLength: 1,
           data: [10, 20, 30, 40, 50, 60, 70],
+          backgroundColor: ["red", "green", "yellow", "blue", "black", "brown", "gray"]
         },
       ],
     },
     options: {
+      plugins: {
+        legend: {
+          labels: {
+            color: "#dc0000",
+            font: {
+              size: 16
+            }
+          }
+        }
+      },
       scales: {
         y: {
           beginAtZero: true,
