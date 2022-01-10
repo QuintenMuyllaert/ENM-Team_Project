af6e72ae6fa431078b4da5b440456697cb3a307e
Jorrit Verfaillie <jorrit.verfaillie@student.howest.be>
2022-01-10 14:42:28 +0100

    module influx

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 const express = require('express');
 const { InfluxDB, Point, HttpError } = require('@influxdata/influxdb-client');
 const { deflateSync } = require('zlib');
+const influx = require('./modules/get_influx.js');
 
 const config = fs.existsSync(path.join(__dirname, 'config.json')) ? require('./config.json') : false;
 const { url, token, org, bucket } = config;
 
-const get_influx = async () => {
+const get_influxs = async () => {
   try {
     const queryApi = new InfluxDB({ url, token }).getQueryApi(org);
     const fluxQuery = `from(bucket: "${bucket}") |> range(start: 2020-01-01T00:00:00Z, stop: 2020-01-01T00:00:20Z)`;
@@ -16,7 +17,7 @@ const get_influx = async () => {
     const data = await api.collectRows(fluxQuery);
     // data.forEach((x) => console.log(JSON.stringify(x)));
     console.log('\nCollect ROWS SUCCESS');
-    return await data;
+    return data;
   } catch (err) {
     console.error(err);
     return;
@@ -33,6 +34,7 @@ app.listen(80, async () => {
     console.log('PLEASE ADD THE CORRECT CONFIG.JSON!!!');
     return;
   }
-  const data = await get_influx();
-  console.log(data);
+  // const data = await get_influx();
+  influx;
+  console.log(influx);
 });
