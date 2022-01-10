cc26dbb579e989c34911b337544c6ac6824b0bdc
MillevilleFlorian <florian.milleville@student.howest.be>
2022-01-10 13:28:22 +0100

    added info

--- www/index.js
+++ www/index.js
@@ -9,13 +9,14 @@ const changeSlide = () => {
   if (!pages.length) {
     return;
   }
-  document.querySelector(".slide-content").innerHTML = pages[slideNr];
+  document.querySelector('.slide-content').innerHTML = pages[slideNr];
   slideNr = slideNr + (1 % pages.length);
 };
 
-document.addEventListener("DOMContentLoaded", async () => {
-  console.log("loaded!");
-  pages.push(await fetchFile("./page1.html"));
+document.addEventListener('DOMContentLoaded', async () => {
+  console.log('loaded!');
+  pages.push(await fetchFile('./page1.html'));
+  pages.push(await fetchFile('./info.html'));
 
   changeSlide();
   let timer = setInterval(() => {
