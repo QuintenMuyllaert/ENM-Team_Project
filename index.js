3c0af25e3d580709bacd38546ea188bef5ecbd5c
Jorrit Verfaillie <jorrit.verfaillie@student.howest.be>
2022-01-10 13:31:36 +0100

    change influx

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ app.use(express.static(path.join(__dirname, 'www')));
 
 app.listen(80, () => {
   console.log('App launched');
-  
+
   const queryApi = new InfluxDB({ url, token }).getQueryApi(org);
   const fluxQuery = `from(bucket: "${bucket}") |> range(start: 2020-01-01T00:00:00Z, stop: 2020-01-01T00:00:20Z)`;
   console.log('*** QUERY ROWS ***');
   queryApi.queryRows(fluxQuery, {
     next(row, tableMeta) {
       const o = tableMeta.toObject(row);
+      console.log(o);
       console.log(`${o._time} ${o._measurement} is '${o._value}': ${o._field}=${o._value}`);
     },
     error(error) {
