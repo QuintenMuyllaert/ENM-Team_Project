48351c0ffaeefdeaa17cdd34e0978d98bcea0ed9
Jorrit Verfaillie <jorrit.verfaillie@student.howest.be>
2022-01-11 11:28:39 +0100

    double quotes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const path = require('path');
-const fs = require('fs');
-const express = require('express');
-const get_influx = require('./modules/get_influx.js');
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+const get_influx = require("./modules/get_influx.js");
 
-const config = fs.existsSync(path.join(__dirname, 'config.json')) ? require('./config.json') : false;
+const config = fs.existsSync(path.join(__dirname, "config.json")) ? require("./config.json") : false;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'www')));
+app.use(express.static(path.join(__dirname, "www")));
 
 app.listen(80, async () => {
-  console.log('App launched');
+  console.log("App launched");
   if (!config) {
-    console.log('PLEASE ADD THE CORRECT CONFIG.JSON!!!');
+    console.log("PLEASE ADD THE CORRECT CONFIG.JSON!!!");
     return;
   }
   const data = await get_influx.run();
