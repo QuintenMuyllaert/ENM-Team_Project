918ac29d0102fdd6f716fac457f951c43cca363c
Quinten Muyllaert <72012305+QuintenMuyllaert@users.noreply.github.com>
2022-01-12 16:07:42 +0100

    cleaned up index.js

--- www/index.js
+++ www/index.js
@@ -25,8 +25,50 @@ const lookupList = (list, includes) => {
   return results;
 };
 
-let pages = [];
-let slideNr = null;
+const generateSlide = (html) => {
+  return `<main class="slide-content">
+        <div class="logo--container">
+          <img src="./img/logo.png" alt="logo" />
+        </div>
+        <div class="slide-content-style">${html}</div>
+      </main>`;
+};
+
+const loopHandle = async () => {
+  await loop();
+  setTimeout(async () => {
+    await loopHandle();
+  }, 3 * 1000);
+};
+
+const loop = async () => {
+  slideNr = (slideNr + 1) % pages.length;
+  if (slideNr == 0) {
+    document.querySelector(".animation--container").classList.add("animation--display");
+    document.querySelector(".animation--logo-container").classList.add("animation--logo-display");
+    await delay(3000);
+
+    document.querySelector(".animation--container").classList.add("animation--display-reverse");
+    document.querySelector(".animation--logo-container").classList.add("animation--logo-display-reverse");
+    document.querySelector(".animation--container").classList.remove("animation--display");
+    document.querySelector(".animation--logo-container").classList.remove("animation--logo-display");
+    window.scroll({
+      top: 0,
+      left: 0,
+    });
+    await delay(3000);
+    document.querySelector(".animation--container").classList.remove("animation--display-reverse");
+    document.querySelector(".animation--logo-container").classList.remove("animation--logo-display-reverse");
+  }
+  window.scroll({
+    top: 0,
+    left: (slideNr / pages.length) * document.body.offsetWidth,
+    behavior: "smooth",
+  });
+};
+
+const pages = [];
+let slideNr = 0;
 
 document.addEventListener("DOMContentLoaded", async () => {
   console.log("loaded!");
@@ -37,73 +79,26 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   document.querySelector(":root").style.setProperty("--pagecount", pages.length);
-  console.log(pages.length);
-
-  let html = "";
-  if (staticSlideNr == -1) {
-    pages.forEach((page) => {
-      console.log("found page");
-      html += `<main class="slide-content">
-        <div class="logo--container">
-          <img src="./img/logo.png" alt="logo" />
-        </div>
-        <div class="slide-content-style">${page}</div>
-      </main>`;
-    });
-    document.querySelector(".main-container").innerHTML = html;
-  } else {
-    document.querySelector(".main-container").innerHTML = `<main class="slide-content">
-        <div class="logo--container">
-          <img src="./img/logo.png" alt="logo" />
-        </div>
-        <div class="slide-content-style">${pages[staticSlideNr]}</div>
-      </main>`;
-  }
-
-  drawChart();
 
   window.scroll({
     top: 0,
     left: 0,
-    behavior: "smooth",
   });
+
+  let html = "";
+  if (staticSlideNr == -1) {
+    pages.forEach((page) => {
+      html += generateSlide(page);
+    });
+    document.querySelector(".main-container").innerHTML = html;
+  } else {
+    document.querySelector(".main-container").innerHTML = generateSlide(pages[staticSlideNr]);
+  }
+
+  drawChart();
 
   if (staticSlideNr != -1) {
     return;
   }
-
-  const loopHandle = async () => {
-    await loop();
-    setTimeout(async () => {
-      await loopHandle();
-    }, 3 * 1000);
-  };
-
-  const loop = async () => {
-    slideNr = (slideNr + 1) % pages.length;
-    if (slideNr == 0) {
-      document.querySelector(".animation--container").classList.add("animation--display");
-      document.querySelector(".animation--logo-container").classList.add("animation--logo-display");
-      await delay(3000);
-
-      document.querySelector(".animation--container").classList.add("animation--display-reverse");
-      document.querySelector(".animation--logo-container").classList.add("animation--logo-display-reverse");
-      document.querySelector(".animation--container").classList.remove("animation--display");
-      document.querySelector(".animation--logo-container").classList.remove("animation--logo-display");
-      window.scroll({
-        top: 0,
-        left: 0,
-      });
-      await delay(3000);
-      document.querySelector(".animation--container").classList.remove("animation--display-reverse");
-      document.querySelector(".animation--logo-container").classList.remove("animation--logo-display-reverse");
-    }
-    window.scroll({
-      top: 0,
-      left: (slideNr / pages.length) * document.body.offsetWidth,
-      behavior: "smooth",
-    });
-  };
-
   loopHandle();
 });
