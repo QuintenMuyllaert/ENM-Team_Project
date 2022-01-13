162ac9598dd17957e56da173132a101417597f24
Quinten Muyllaert <72012305+QuintenMuyllaert@users.noreply.github.com>
2022-01-13 15:26:09 +0100

    fixed piechart code

--- www/piechart.js
+++ www/piechart.js
@@ -3,33 +3,37 @@ const rngHex = () => {
   return a.length == 1 ? "0" + a : a;
 };
 
-const drawPie = () => {
-  const piechart = document.querySelector(".piechart");
+const drawPie = (data = [25, 25, 20, 15, 5, 5, 5]) => {
+  let colors = ["#FF0000"];
 
-  let data = [25, 25, 25, 10, 10, 5];
-  for (let p of data) {
+  data = data.reverse();
+  for (let i = 0; i < data.length; i++) {
+    colors.push(Math.round(i * 16777215) / data.length);
+  }
+
+  const piechart = document.querySelector(".piechart");
+  for (let i in data) {
     piechart.innerHTML += `<circle class="duiktank--circle js-pie" data-name="duiktank"></circle>`;
   }
 
   let sum = 0;
-  let points = [];
+  const points = [];
   const children = piechart.querySelectorAll(".js-pie");
-  for (let i in children) {
+
+  data.unshift(0);
+  for (let i in data) {
     duiktank = 100 - (data[i] + sum);
     points.push(duiktank);
     sum += data[i];
   }
+  points.pop();
 
-  for (let i in children) {
-    let piece = children[i];
-
+  for (let i in points) {
+    const piece = children[i];
     duiktank = 100 - points[i];
 
-    try {
-      piece.style.stroke = `#${rngHex() + rngHex() + rngHex()}`;
-      percentage = (duiktank / 100) * 283.14;
-      piece.style.strokeDashoffset = percentage;
-    } catch (e) {}
-    //283.140 is de top en zorgt voor niks van percentage (100%)
+    piece.style.stroke = colors[i];
+    percentage = (duiktank / 100) * 283.14; //283.140 is de top en zorgt voor niks van percentage (100%)
+    piece.style.strokeDashoffset = percentage;
   }
 };
