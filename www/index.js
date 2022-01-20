de0437c09ecfbddffdd61f1b4e84fd4c664ea23f
Quinten Muyllaert <72012305+QuintenMuyllaert@users.noreply.github.com>
2022-01-20 12:23:21 +0100

    Socket code cleaning

--- www/index.js
+++ www/index.js
@@ -12,47 +12,6 @@ let didyouknow = [];
 
 let day = 0;
 let night = 0;
-socket.emit("data", 1);
-
-socket.on("Influx", (data) => {
-  console.log(data);
-
-  for (waarde of data.TotaalNet) {
-    const time = parseInt(waarde._time.split("T")[1].split(":")[0]);
-    if (time >= 22 || time < 6) {
-      night += waarde._value;
-    } else {
-      day += waarde._value;
-    }
-  }
-  night = night / 1000;
-  day = day / 1000;
-  document.querySelector(".js-day").innerText = `Verbruik dag: ${day.toFixed(2)} kW`;
-  document.querySelector(".js-night").innerText = `Verbruik nacht: ${night.toFixed(2)} kW`;
-  const total = day + night;
-  document.querySelector(".js-oneday").innerText = `${total.toFixed(2)}`;
-
-  console.log(day, night);
-  drawChartDayNight([day, night]);
-});
-
-socket.emit("data", 7);
-socket.on("Influx_week", (data) => {
-  let night_week = 0;
-  let day_week = 0;
-  for (waarde of data.TotaalNet) {
-    const time = parseInt(waarde._time.split("T")[1].split(":")[0]);
-    if (time >= 22 || time < 6) {
-      night_week += waarde._value;
-    } else {
-      day_week += waarde._value;
-    }
-  }
-  night_week = night_week / 1000;
-  day_week = day_week / 1000;
-  document.querySelector(".js-dagweek").innerText = `${day_week.toFixed(2)}`;
-  document.querySelector(".js-nightweek").innerText = `${night_week.toFixed(2)}`;
-});
 
 const delay = (time) => {
   return new Promise((resolve) => setTimeout(resolve, time));
