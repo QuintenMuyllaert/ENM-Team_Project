0d044fae39b8943e6d6b2703cfdcec020dfa2ba4
Quinten Muyllaert <72012305+QuintenMuyllaert@users.noreply.github.com>
2022-01-21 00:07:11 +0100

    moved some global functions to util to make index more readable

--- www/index.js
+++ www/index.js
@@ -18,51 +18,8 @@ const elementNumberNightWeek = new dataElement(".js-nightweek", 0, elementDefaul
 
 const elementChartDayNight = new dataElement(".js-day-night", [0, 0], { ...elementDefaultsChart, init: chartInitDayNight });
 
-const delay = (time) => {
-  return new Promise((resolve) => setTimeout(resolve, time));
-};
-
-const fetchString = async (url) => {
-  const data = await fetch(url);
-  return await data.text();
-};
-
-const fetchJSON = async (url) => {
-  const data = await fetch(url);
-  return await data.json();
-};
-
-const fetchTxt = async (url) => {
-  const data = await fetchString(url);
-  return data.split("\n");
-};
-
-const lookupList = (list, includes) => {
-  const results = [];
-  list = Object.keys(list);
-  for (const item of list) {
-    if (item.includes(includes)) {
-      results.push(item);
-    }
-  }
-  return results;
-};
-
 const generateSlide = (html) => {
   return skeletonSlide.replace("<!--INNERHTML-->", html);
-};
-
-const addClassRemoveAfter = (element, className, time) => {
-  element.classList.add(className);
-  setTimeout(() => {
-    element.classList.remove(className);
-  }, time);
-};
-
-const triggerClass = async (element, className) => {
-  element.classList.remove(className);
-  await delay(10);
-  element.classList.add(className);
 };
 
 const renderDidYouKnow = async () => {
