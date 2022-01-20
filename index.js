f4626e668aa0b71d690e0b4ae8f7b6d646f96315
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-01-20 18:52:54 +0100

    Colors, console, names and cleanup

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const { Server } = require("socket.io");
 const speakeasy = require("speakeasy");
 const qrcode = require("qrcode");
 
-const get_influx = require("./modules/get_influx.js");
+const console = require("./modules/console.js");
+const influx = require("./modules/influx.js");
 const tree = require("./modules/tree.js");
 const mqtt = require("./modules/mqtt.js");
 
 const config = fs.existsSync(path.join(__dirname, "config.json")) ? require("./config.json") : false;
-const { bucket } = config;
+console.log("Starting ENM-G2 Team_Project!\nMade possible by :\n - Quinten Muyllaert\n - Toby Bostoen\n - Jorrit Verfaillie\n - Florian Milleville\n");
 
 const app = express();
 const server = http.createServer(app);
@@ -30,23 +31,25 @@ if (config.topic) {
   mqtt.subscribe(config.topic);
 }
 
-get_influx.fetchPeriodically(io);
+influx.fetchPeriodically(io);
 
 app.use(express.static(path.join(__dirname, "www")));
 
 app.get("/tree.json", async (req, res) => {
+  console.log("Client requested ./www filetree.");
   res.send(JSON.stringify(await tree(path.join(__dirname, "www")), null, 4));
 });
 
 io.on("connection", async (socket) => {
   socket.auth = false;
 
-  console.log(`A user connected to the server : "${socket.id}"`);
+  console.log(`A user connected to the server : "${socket.id}".`);
   socket.on("disconnect", () => {
-    console.log(`User disconnected : ${socket.id}`);
+    console.log(`User disconnected : "${socket.id}".`);
   });
 
   socket.on("forget", async (code) => {
+    console.log("Client sent a forgot password request.");
     const verfied = speakeasy.totp.verify({
       secret: `${config.twofactor}`,
       encoding: "ascii",
@@ -56,25 +59,28 @@ io.on("connection", async (socket) => {
   });
 
   socket.on("auth", async (obj) => {
+    console.log("Client attempts to authenticate.");
     if (!obj) {
+      console.log("Authentication failed, no object.");
       socket.emit("auth", false);
       return;
     }
+
     if (!(obj.username && obj.password)) {
+      console.log("Authentication failed, wrong credentials.");
       socket.emit("auth", false);
       return;
     }
 
     if (!config.username || !config.password) {
-      console.log("No username and/or password provided in config.");
-
+      console.log("No username and/or password provided in config, registering user!");
       config.username = await bcrypt.hash(obj.username, await bcrypt.genSalt(10));
       config.password = await bcrypt.hash(obj.password, await bcrypt.genSalt(10));
-
       fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
     }
 
     if (!config.twofactor) {
+      console.log("No twofactor secret in config, generating twofactor secret!");
       const secret = speakeasy.generateSecret({
         name: "Transfo_Recovery",
       });
@@ -86,22 +92,24 @@ io.on("connection", async (socket) => {
     }
 
     if ((await bcrypt.compare(obj.username, config.username)) && (await bcrypt.compare(obj.password, config.password))) {
+      console.log("Authentication successfull!");
       socket.emit("auth", true);
       socket.auth = true;
     } else {
+      console.log("Authentication failed, wrong credentials.");
       socket.emit("auth", false);
       socket.auth = false;
     }
   });
 
-  if (!Object.keys(get_influx.lastHour).length || !Object.keys(get_influx.lastHour).length) {
+  if (!Object.keys(influx.lastHour).length || !Object.keys(influx.lastHour).length) {
     console.log("Fetching Influx data...");
-    await get_influx.fetch(socket, 1);
-    await get_influx.fetch(socket, 7);
+    await influx.fetch(socket, 1);
+    await influx.fetch(socket, 7);
   } else {
     console.log("Sending cached Influx data...");
-    socket.emit("Influx", get_influx.lastHour);
-    socket.emit("Influx_week", get_influx.lastWeek);
+    socket.emit("influx", influx.lastHour);
+    socket.emit("influxWeek", influx.lastWeek);
   }
 
   socket.on("error", (err) => console.error);
--- modules/influx.js
+++ modules/influx.js
@@ -1,44 +1,50 @@
 const path = require("path");
 const fs = require("fs");
-const { InfluxDB, HttpError } = require("@influxdata/influxdb-client");
+const { InfluxDB } = require("@influxdata/influxdb-client");
 const config = fs.existsSync(path.join(__dirname, "../config.json")) ? require("../config.json") : false;
-const { bucket, url, token, org } = config;
 
 module.exports = {
+  lastHour: {},
+  lastWeek: {},
   run: async (querry) => {
     try {
-      console.log("Reading");
+      const { url, token, org } = config;
       if (!url) {
         console.log("NO URL PROVIDED FOR INFLUXDB!");
         return;
       }
+
+      console.log("Connecting.");
       const queryApi = new InfluxDB({ url, token }).getQueryApi(org);
-      const fluxQuery = querry;
-      console.log("*** QUERY ROWS ***");
-      const api = await queryApi;
-      const data = await api.collectRows(fluxQuery);
-      // data.forEach((x) => console.log(JSON.stringify(x)));
-      console.log("\nCollect ROWS SUCCESS");
+      console.log("Executing  querry.");
+      const data = await queryApi.collectRows(querry);
+      console.log("Received data!");
       return data;
     } catch (err) {
       console.error(err);
-      return;
+      return false;
     }
   },
-  lastHour: {},
-  lastWeek: {},
   fetch: async (io, days) => {
     if (!config) {
       console.log("PLEASE ADD THE CORRECT CONFIG.JSON!!!");
       return;
     }
+    console.log("Attempting to fetch data from InfluxDB.");
+
     let today = new Date(Date.now());
     today.setHours(1, 0, 0, 0);
     const stopdate = today.toISOString();
     today = today.minusDays(days);
     const startdate = today.toISOString();
-    const querry = `from(bucket: "${bucket}") |> range(start: ${startdate}, stop: ${stopdate}) |> aggregateWindow(every: 1h, fn: last, createEmpty: false) `;
+    const querry = `from(bucket: "${config.bucket}") |> range(start: ${startdate}, stop: ${stopdate}) |> aggregateWindow(every: 1h, fn: last, createEmpty: false) `;
     const data = await module.exports.run(querry);
+
+    if (!data) {
+      console.log("Something went wrong while fetching data!\nGot empty data object, possibly because the database is offline.");
+      return;
+    }
+
     const ret = {};
     for (const thing of data) {
       if (!ret[thing._field]) {
@@ -49,17 +55,21 @@ module.exports = {
     }
     if (days == 1) {
       module.exports.lastHour = ret;
-      io.emit("Influx", module.exports.lastHour);
+      io.emit("influx", module.exports.lastHour);
+      console.log(`Pushed data to Socket.IO on topic "influx"!`);
     }
     if (days == 7) {
       module.exports.lastWeek = ret;
-      io.emit("Influx_week", module.exports.lastWeek);
+      io.emit("influxWeek", module.exports.lastWeek);
+      console.log(`Pushed data to Socket.IO on topic "influxWeek"!`);
     }
   },
   fetchPeriodically: async (io) => {
+    console.log("First periodical fetch of the data.");
     await module.exports.fetch(io, 1);
     await module.exports.fetch(io, 7);
     setInterval(async () => {
+      console.log("Periodical fetch of the data.");
       await module.exports.fetch(io, 1);
       await module.exports.fetch(io, 7);
     }, 10 * 60 * 1000);
