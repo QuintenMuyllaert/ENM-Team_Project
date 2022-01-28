06f64e95ae932e2c623da4003041b0bd12df3155
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-01-28 17:30:28 +0100

    influx data rework

--- modules/influx.js
+++ modules/influx.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const { InfluxDB } = require("@influxdata/influxdb-client");
 const config = fs.existsSync(path.join(__dirname, "../config.json")) ? require("../config.json") : false;
+const { getSunset, getSunrise } = require("sunrise-sunset-js");
 
 module.exports = {
   lastHour: {},
@@ -13,6 +14,15 @@ module.exports = {
       console.log("NO URL PROVIDED FOR INFLUXDB!");
       return;
     }
+    console.log("Setting basisy.");
+    module.exports.oneSecond = 1;
+    module.exports.oneMinute = 60 * module.exports.oneSecond;
+    module.exports.oneHour = 60 * module.exports.oneMinute;
+    module.exports.oneDay = 24 * module.exports.oneHour;
+    module.exports.oneWeek = 7 * module.exports.oneDay;
+    module.exports.oneMonth = 30.4368499 * module.exports.oneDay;
+    module.exports.oneYear = 365.242199 * module.exports.oneDay;
+
     console.log("Connecting.");
     module.exports.queryApi = new InfluxDB({ url, token }).getQueryApi(org);
   },
@@ -32,120 +42,144 @@ module.exports = {
       return false;
     }
   },
-  fetch: async (io, days) => {
+  calculateDayNight: (data, basis) => {
+    //hours (server time) we see as morning / evening
+    //morning <= time < evening = day
+    //!day = night
+    const cords = [50.81268, 3.33848];
+    const morning = new Date(getSunrise(...cords)).getHours();
+    const evening = new Date(getSunset(...cords)).getHours();
+
+    let listdag = 0;
+    let listnacht = 0;
+
+    let lenDag = 0;
+    let lenNacht = 0;
+
+    for (const thing of data) {
+      if (thing.time) {
+        const time = parseInt(thing.time.split("T")[1].split(":")[0]);
+        if (time < evening && time >= morning) {
+          listdag += thing.val;
+          lenDag++;
+        } else {
+          listnacht += thing.val;
+          lenNacht++;
+        }
+      }
+    }
+
+    return {
+      day: {
+        total: (listdag / lenDag) * (basis / module.exports.oneHour),
+        avg: listdag / lenDag,
+      },
+      night: {
+        total: (listnacht / lenNacht) * (basis / module.exports.oneHour),
+        avg: listnacht / lenNacht,
+      },
+    };
+  },
+  sanitize: async (io) => {
+    const timelabels = ["minuit", "uur", "dag", "week", "maand", "jaar"];
+    const basisy = [module.exports.oneMinute, module.exports.oneHour, module.exports.oneHour, module.exports.oneHour, module.exports.oneDay, module.exports.oneMonth];
+    const amt = [1 / 24 / 60, 1 / 24, 1, 8, 31, 357];
+
+    const dataByMeter = {};
+    for (const i in timelabels) {
+      const basis = basisy[i];
+      const label = timelabels[i];
+      const data = await module.exports.fetch(amt[i], basis);
+
+      for (const thing of data) {
+        const obj = {
+          val: thing._value,
+          time: thing._time,
+        };
+        if (!dataByMeter[thing._field]) {
+          dataByMeter[thing._field] = {};
+        }
+        if (!dataByMeter[thing._field][label]) {
+          dataByMeter[thing._field][label] = { data: [obj], basis: label };
+        } else {
+          dataByMeter[thing._field][label].data.unshift(obj);
+        }
+      }
+
+      const meters = Object.keys(dataByMeter);
+      for (const meter of meters) {
+        let sum = 0;
+        const len = dataByMeter[meter][label].data.length;
+        for (const point of dataByMeter[meter][label].data) {
+          sum += point.val;
+        }
+        const avg = sum / len; // = W
+        const tot = avg * (basis / module.exports.oneHour); // = W/basis
+        dataByMeter[meter][label].gemiddeld = avg;
+        dataByMeter[meter][label].totaal = tot;
+        if (basis == module.exports.oneHour) {
+          const dayNight = module.exports.calculateDayNight(dataByMeter[meter][label].data, basisy);
+          dataByMeter[meter][label].overdag = {
+            gemiddeld: dayNight.night.avg,
+            totaal: dayNight.night.total,
+          };
+          dataByMeter[meter][label].snachts = {
+            gemiddeld: dayNight.day.avg,
+            totaal: dayNight.day.total,
+          };
+        }
+      }
+    }
+
+    const meters = Object.keys(dataByMeter);
+    for (const meter of meters) {
+      //#Blame Senne & CO
+      if (!dataByMeter[meter]["dag"]) {
+        delete dataByMeter[meter];
+      }
+    }
+
+    console.log("Sending data to socket");
+    module.exports.data = dataByMeter;
+    io.emit("influxData", dataByMeter);
+  },
+  secondsToDays: (s) => {
+    const oneDayInSeconds = 60 * 60 * 24;
+    return s / oneDayInSeconds;
+  },
+  fetch: async (days, basis) => {
     if (!config) {
       console.log("PLEASE ADD THE CORRECT CONFIG.JSON!!!");
       return;
     }
     console.log("Attempting to fetch data from InfluxDB.");
-
+    console.log(days);
     let today = new Date(Date.now());
     today.setHours(1, 0, 0, 0);
     const stopdate = today.toISOString();
     today = today.minusDays(days);
     const startdate = today.toISOString();
-    const querry = `from(bucket: "${config.bucket}") |> range(start: ${startdate}, stop: ${stopdate}) |> aggregateWindow(every: 1h, fn: last, createEmpty: false) `;
+    const querry = `from(bucket: "${config.bucket}") |> range(start: ${startdate}, stop: ${stopdate}) |> aggregateWindow(every: ${Math.round(basis)}s, fn: mean, createEmpty: false) `;
     const data = await module.exports.run(querry);
 
     if (!data) {
       console.log("Something went wrong while fetching data!\nGot empty data object, possibly because the database is offline.");
       return;
     }
-    let number = 0;
-    const raw = {};
-    for (const thing of data) {
-      if (!raw[thing._field]) {
-        raw[thing._field] = [thing._value];
-      } else {
-        raw[thing._field].push(thing._value);
-      }
-    }
-    const ret = {};
 
-    //hours (server time) we see as morning / evening
-    //morning <= time < evening = day
-    //!day = night
-    const morning = 6;
-    const evening = 22;
-
-    //totaal dag+nacht ( 24 waardes - subject to change make dynamic )
-    for (const thing of data) {
-      if (!ret[thing._field]) {
-        number = 0;
-        ret[thing._field] = thing._value;
-        number += thing._value;
-      } else {
-        number += thing._value;
-        ret[thing._field] = number;
-      }
-    }
-
-    //totaal dag ( 16 waardes - subject to change make dynamic )
-    const listdag = {};
-    const listnacht = {};
-    for (const thing of data) {
-      const time = parseInt(thing._time.split("T")[1].split(":")[0]);
-      if (!listdag[thing._field]) {
-        number = 0;
-        if (time < evening && time >= morning) {
-          listdag[thing._field] = thing._value;
-          number += thing._value;
-        }
-      } else {
-        if (time < evening && time >= morning) {
-          number += thing._value;
-          listdag[thing._field] = number;
-        }
-      }
-    }
-
-    //totaal nacht ( 8 waardes - subject to change make dynamic )
-    for (const thing of data) {
-      const time = parseInt(thing._time.split("T")[1].split(":")[0]);
-      if (!listnacht[thing._field]) {
-        number = 0;
-        if (time >= evening || time < morning) {
-          listnacht[thing._field] = thing._value;
-          number += thing._value;
-        }
-      } else {
-        if (time >= evening || time < morning) {
-          number += thing._value;
-          listnacht[thing._field] = number;
-        }
-      }
-    }
-    //rawdata
-    module.exports.raw = raw;
-    io.emit("influxraw", module.exports.raw);
-    //dag
-    if (days == 1) {
-      module.exports.lastdaytotal = ret;
-      module.exports.lastday_day = listdag;
-      module.exports.lastday_night = listnacht;
-      io.emit("influxtotalDay", module.exports.lastdaytotal);
-      io.emit("influxDay", module.exports.lastday_day, module.exports.lastday_night);
-      console.log(`Pushed data to Socket.IO on topic "influx"!`);
-    }
-
-    //week
-    if (days == 7) {
-      module.exports.lastWeektotal = ret;
-      module.exports.lastweek_day = listdag;
-      module.exports.lastweek_night = listnacht;
-      io.emit("influxtotalWeek", module.exports.lastWeektotal);
-      io.emit("influxWeek", module.exports.lastweek_day, module.exports.lastweek_night);
-      console.log(`Pushed data to Socket.IO on topic "influxWeek"!`);
-    }
+    return data;
   },
   fetchPeriodically: async (io) => {
     console.log("First periodical fetch of the data.");
-    await module.exports.fetch(io, 1);
-    await module.exports.fetch(io, 7);
+    //await module.exports.fetch(io, 1);
+    //await module.exports.fetch(io, 7);
+
+    await module.exports.sanitize(io);
     setInterval(async () => {
       console.log("Periodical fetch of the data.");
-      await module.exports.fetch(io, 1);
-      await module.exports.fetch(io, 7);
-    }, 5000);
+      await module.exports.sanitize(io);
+      //await module.exports.fetch(io, 1);
+      //await module.exports.fetch(io, 7);
+    }, 60 * 1000);
   },
 };
