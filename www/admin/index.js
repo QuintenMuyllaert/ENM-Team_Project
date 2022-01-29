729d193b8a737e93e1baf8e3bf5426fb872bdfa9
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-01-29 19:04:29 +0100

    switch between all pages in slide editor

--- www/admin/index.js
+++ www/admin/index.js
@@ -9,6 +9,9 @@ let passwordShow = false;
 let htmlUsername, htmlPassword, htmlButtonAuth, htmlButtonShowPassword, htmlControlDidYouKnow, htmlControlHomeSlideLength, htmlControlHomeStaticSlide, htmlControlHome, htmlNavlist;
 let pageFunction = {};
 
+let pageNrToEdit = 0;
+let tree = {};
+
 pageFunction["./control/slide.html"] = async () => {
   console.log("en zo link je functions die moeten starten als je setting inload");
 };
@@ -20,12 +23,24 @@ const generateSlide = (html) => {
 
 setInterval(() => {
   dataElements.forEach(async (e) => {
-    e.render();
+    e.tick();
+    e.update();
   });
-}, 1000);
+}, 100);
 
-pageFunction["./control/press.html"] = async (file) => {
-  const fileName = "../slide/duiktank.html";
+pageFunction["./control/press.html"] = async () => {
+  if (lookupList(tree["slide"], ".html")[pageNrToEdit] == undefined) {
+    console.log("out of bounds");
+    if (pageNrToEdit < 0) {
+      pageNrToEdit = lookupList(tree["slide"], ".html").length - 1;
+    } else {
+      pageNrToEdit = 0;
+    }
+  }
+  const fileName = "../slide/" + lookupList(tree["slide"], ".html")[pageNrToEdit];
+
+  document.querySelector(".press--number").textContent = pageNrToEdit;
+
   skeletonSlide = await fetchString("../skeletonSlide.html");
   document.querySelector(".press--slide-viewport").innerHTML = generateSlide(await fetchString(fileName)).replace(/\.\//g, "../");
   const height = document.querySelector(".press--slide-container").offsetHeight;
@@ -33,41 +48,103 @@ pageFunction["./control/press.html"] = async (file) => {
   const scalefactor = height / 1080;
   document.querySelector(".press--slide-scale").style.transform = `scale(${scalefactor},${scalefactor})`;
 
-  const htmlSlide = document.querySelector(".press--slide-scale").querySelector(".main");
+  const htmlSlide = document.querySelector(".press--slide-scale").querySelector(".slide--root");
   const textTags = ["P", "H1", "H2", "H3", "H4", "H5", "A", "SPAN", "BUTTON"];
 
+  let selected = null;
   setInterval(() => {
-    if (!document.querySelector(".press--outline")) {
+    if (!selected) {
       return;
     }
-    if (!textTags.includes(document.querySelector(".press--outline").tagName)) {
+    if (!textTags.includes(selected.tagName)) {
       return;
     }
-    document.querySelector(".press--outline").textContent = document.querySelector(".press-selected-item").value;
+    if (selected.getAttribute("dataElement") != null) {
+      return;
+    }
+    selected.textContent = document.querySelector(".press-selected-item").value;
   }, 50);
+
+  setInterval(() => {
+    if (!selected) {
+      return;
+    }
+    if (selected.getAttribute("dataElement") == null) {
+      return;
+    }
+    selected.setAttribute("dataValue", document.querySelector("#toolbox--dataValue").value);
+    selected.setAttribute("dataType", document.querySelector("#toolbox--dataType").value);
+  }, 50);
+
+  let htmlCheckboxVis = document.querySelector("input#toolbox--visible");
+  htmlCheckboxVis.addEventListener("click", () => {
+    console.log(htmlCheckboxVis.checked);
+    htmlCheckboxVis.checked = true;
+  });
+
   htmlSlide.addEventListener("click", (event) => {
-    console.log(event.target);
     document.querySelectorAll(".press--outline").forEach((e) => e.classList.remove("press--outline"));
-    event.target.classList.add("press--outline");
     if (textTags.includes(event.target.tagName)) {
       document.querySelector(".press-selected-item").value = event.target.textContent;
+      event.target.classList.add("press--outline");
+      selected = event.target;
     } else {
       console.log("not text");
+    }
+    let dataElement = null;
+    if (event.target.getAttribute("dataElement") != null) {
+      console.log("Self is dataElement");
+      dataElement = event.target;
+    } else {
+      let e = event.target.parentNode;
+      while (e.getAttribute("dataElement") == null) {
+        e = e.parentNode;
+        if (e.tagName == "BODY") {
+          console.log("No dataElement in tree.");
+          break;
+        }
+      }
+      if (e.getAttribute("dataElement") != null) {
+        console.log("Parent is dataElement");
+        dataElement = e;
+      }
+    }
+    if (dataElement != null) {
+      selected = dataElement;
+      selected.classList.add("press--outline");
+
+      document.querySelector("#toolbox--dataType").value = dataElement.getAttribute("dataType");
+      document.querySelector("#toolbox--dataValue").value = dataElement.getAttribute("dataValue");
+    } else {
+      event.target.classList.add("press--outline");
+      selected = event.target;
     }
   });
 
   const uploader = new SocketIOFileUpload(socket);
   uploader.listenOnInput(document.getElementById("siofu_input"));
   uploader.addEventListener("complete", (e) => {
-    console.log(e);
-    document.querySelector(".item--big-image").src = "../upload/" + e.file.name;
+    selected.src = "../upload/" + e.file.name;
   });
 
   document.querySelector(".press--undo").addEventListener("click", async () => {
     console.log("Undo-ing changes.");
     document.querySelector(".admin--page-container").innerHTML = await fetchString("./control/press.html");
-    pageFunction["./control/press.html"](file);
+    await pageFunction["./control/press.html"]();
   });
+
+  document.querySelector(".press--goforward").addEventListener("click", async () => {
+    pageNrToEdit++;
+    document.querySelector(".admin--page-container").innerHTML = await fetchString("./control/press.html");
+    await pageFunction["./control/press.html"]();
+  });
+
+  document.querySelector(".press--goback").addEventListener("click", async () => {
+    pageNrToEdit--;
+    document.querySelector(".admin--page-container").innerHTML = await fetchString("./control/press.html");
+    await pageFunction["./control/press.html"]();
+  });
+
   document.querySelector(".press--save").addEventListener("click", async () => {
     console.log("Saving changes.");
     const htmlPage = document.querySelector(".press--slide-viewport").querySelector(".slide--root");
@@ -161,6 +238,8 @@ const loadPageContent = async (e) => {
 
 //Gets called as soon as client has logged in and the page is loaded.
 const init = async () => {
+  tree = await fetchJSON("../tree.json");
+
   //Queryselectors
   htmlNavlist = document.querySelector(".admin--nav-list");
 
