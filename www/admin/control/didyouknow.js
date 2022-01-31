e58f8bdd0554eb27335ac6340715913fa3379684
Jorrit Verfaillie <jorrit.verfaillie@student.howest.be>
2022-01-31 13:23:18 +0100

    dyk 2

--- www/admin/control/didyouknow.js
+++ www/admin/control/didyouknow.js
@@ -2,6 +2,7 @@ pageFunction["./control/didyouknow.html"] = async () => {
   const facts = await fetchTxt("../data/facts.csv");
   const factsPreview = document.querySelector(".weetje");
   const factsPlaceholder = document.querySelector(".dyk--container-items");
+  const newfact = document.querySelector(".dyk--item-add");
   let factsString = "";
   let i = 0;
   for (let fact of facts) {
@@ -26,6 +27,34 @@ pageFunction["./control/didyouknow.html"] = async () => {
       document.getElementById(id).remove();
     });
   }
+
+  newfact.addEventListener("click", () => {
+    factsPlaceholder.innerHTML += `<div class="dyk--item">
+    <input id="${i}" value="" class="dyk--item-text"></>
+    <div id="del${i}"  class="dyk--item-delete">
+      <svg class="dyk--item--delete" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 579.13 579.13"><defs><style>.a{fill:#1d1d1b;}.a,.b{stroke:#1d1d1b;stroke-miterlimit:10;}.b{fill:#1d1d1b;}</style></defs><path class="a" d="M548.1,169.29C388.46,169.29,259,298.71,259,458.35S388.46,747.42,548.1,747.42,837.17,618,837.17,458.35,707.75,169.29,548.1,169.29Zm0,548.05c-143,0-259-116-259-259s116-259,259-259,259,116,259,259S691.14,717.34,548.1,717.34Z" transform="translate(-258.54 -168.79)"/><rect class="b" x="148.94" y="255.19" width="281.25" height="68.75"/></svg>
+    </div>
+  </div>`;
+    i++;
+    const factsHTML = document.querySelectorAll(".dyk--item-text");
+    for (let fact of factsHTML) {
+      fact.addEventListener("click", () => {
+        factsPreview.innerHTML = fact.value;
+      });
+    }
+    for (item of document.querySelectorAll(".dyk--item-delete")) {
+      item.addEventListener("click", (item) => {
+        console.log("click");
+
+        const facts = item.srcElement;
+        const id = facts.id.split("del")[1];
+        facts.remove();
+        console.log(document.getElementById(id).value);
+        document.getElementById(id).remove();
+      });
+    }
+  });
+
   for (item of document.querySelectorAll(".dyk--item-text")) {
     console.log(item.defaultValue);
   }
