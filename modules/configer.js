288e4f5458ba27c64327f31209b756b8286d8524
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-01-31 18:35:33 +0100

    scaling function is always on

--- modules/configer.js
+++ modules/configer.js
@@ -3,7 +3,7 @@ const path = require("path");
 const configFrontend = fs.existsSync(path.join(__dirname, "../www/config.json")) ? require("../www/config.json") : false;
 module.exports = {
   generateFrontend: () => {
-    const defaultsFrontend = { staticSlideNr: -1, showEndAnimation: true, useScalingFunction: true, slideLength: 15, endAnimationLength: 5 };
+    const defaultsFrontend = { staticSlideNr: -1, showEndAnimation: true, slideLength: 15, endAnimationLength: 5 };
     if (!configFrontend) {
       console.log("Generating frontend config file.");
       fs.writeFileSync("./www/config.json", JSON.stringify(defaultsFrontend, null, 4));
